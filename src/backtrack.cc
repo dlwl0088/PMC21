5050a4809543b1641088cb346e6193b05b723f90
dlwl0088@seoul.ac.kr <dlwl0088@seoul.ac.kr>
2021-06-01 18:59:48 +0900

    iteration 수정

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -17,60 +17,42 @@ void Backtrack::PrintAllMatches(const Graph& data, const Graph& query,
 
 	std::cout << "t " << Nq << "\n";
 
+	std::vector<Vertex> embd(Nq, -1); //  Store partial embedding
+	std::vector<bool> isembd(Nd, false); // Avoid inserting overlapping vertices into embd	
 	std::vector<bool> mark(Nq, false); // Mark whether vertices of query is visited or not
-	std::vector<bool> inserted(Nq, false); // Avoid inserting overlapping vertices into heap
-	std::vector<bool> isembd(Nd, false); // Avoid inserting overlapping vertices into embd
-
-	std::vector<Vertex> embd(Nq, -1);
-	size_t len = 0; // length of current partial embedding
-
-	std::vector<Vertex> state(Nq); // Store recurrence state 
-
-
 	std::vector<size_t> csPos(Nq); // the current position in the candidate set of each vertex
 
+	std::vector<Vertex> state(Nq); // Store recurrence state 
+	size_t len = 0; // length of current partial embedding
+
 	std::vector<size_t> measure(Nq); // the measure for selecting extendable vertices
-
-
 	Heap extVtx(Nq, measure); // Store extendable vertices
+	std::vector<bool> inserted(Nq, false); // Avoid inserting overlapping vertices into heap
 
 	Vertex u = 0; // current vertex in the query
 	Vertex un, v;
 
 	// Selecting root vertex
-	long double argmin = (cs.GetCandidateSize(0) / ((long double)query.GetDegree(0))), argtemp;
+
+	long double argmin = cs.GetCandidateSize(0) / ((long double)query.GetDegree(0)), argtemp;
 
 	for (size_t i = 1; i < Nq; i++) {
-		argtemp = (cs.GetCandidateSize(i) / ((long double)query.GetDegree(i)));
+		argtemp = cs.GetCandidateSize(i) / ((long double)query.GetDegree(i));
 		if (argtemp < argmin) {
 			u = i;
 			argmin = argtemp;
 		}
 	}
 
-
-	state[len++] = u;
-
 	bool embedded_u, embedded_v;
 
 	bool call = true; //Store whether the function is examining new vertex or backtracking
 
-	v = cs.GetCandidate(u, 0);
-	embd[u] = v;
-	isembd[v] = true;
-	csPos[u] = 1;
-	mark[u] = true;
+	measure[u] = cs.GetCandidateSize(u);
+	extVtx.insert(u);
+	inserted[u] = true;
 
-	for (size_t k = query.GetNeighborStartOffset(u); k < query.GetNeighborEndOffset(u); k++) {
-		un = query.GetNeighbor(k);
-		measure[un] = (cs.GetCandidateSize(un) - csPos[un]);
-		extVtx.insert(un);
-		inserted[un] = true;
-	}
-
-
-
-	while (len > 0) {
+	do {
 		if (len == Nq) {
 			std::cout << "a";
 			for (size_t i = 0; i < Nq; i++)
@@ -78,40 +60,26 @@ void Backtrack::PrintAllMatches(const Graph& data, const Graph& query,
 				std::cout << " " << embd[i];
 			}
 			std::cout << "\n";
+			call = false;
+		}
 
+		if (call) {
+			u = extVtx.remove();
+			inserted[u] = false;
+		}
+		else {
+			u = state[--len];
+			mark[u] = false;
 			isembd[embd[u]] = false;
 			embd[u] = -1;
-			mark[u] = false;
-			len--;
+			//std::cout << u << " " << csPos[u] << " " << cs.GetCandidateSize(u) << std::endl;
 		}
 
-		else {
-			if (call) {
-				u = extVtx.remove();
-				inserted[u] = false;
-			}
-			else {
-				if (embd[u] != -1) isembd[embd[u]] = false;
-				embd[u] = -1;
-				measure[u] = (cs.GetCandidateSize(u) - csPos[u]);
-				extVtx.insert(u);
-				inserted[u] = true;
 
-				len--;
-
-				u = state[len];
-				mark[u] = false;
-				isembd[embd[u]] = false;
-				embd[u] = -1;
-				//std::cout << u << " " << csPos[u] << " " << cs.GetCandidateSize(u) << std::endl;
-			}
-		}
 		//for (size_t j = 0; j < len; j++) std::cout << state[j] << " ";
 		//std::cout << "\n";
 
 		embedded_u = false;
-
-
 
 		for (size_t j = csPos[u]; j < cs.GetCandidateSize(u); j++) {
 			v = cs.GetCandidate(u, j);
@@ -124,7 +92,7 @@ void Backtrack::PrintAllMatches(const Graph& data, const Graph& query,
 
 			if (len < query.GetDegree(u)) {
 				for (size_t pos = 0; pos < len; pos++) {
-					if (!(data.IsNeighbor(embd[state[pos]], v))) {
+					if (data.IsNeighbor(state[pos], u) && !(data.IsNeighbor(embd[state[pos]], v))) {
 						embedded_v = false;
 						break;
 					}
@@ -140,20 +108,21 @@ void Backtrack::PrintAllMatches(const Graph& data, const Graph& query,
 				}
 			}
 
-
 			if (embedded_v) {
 				embd[u] = v;
 				isembd[v] = true;
 				csPos[u] = j + 1;
+				mark[u] = true;
+
 				state[len++] = u;
 				embedded_u = true;
-				mark[u] = true;
+
 				call = true;
 
 				for (size_t k = query.GetNeighborStartOffset(u); k < query.GetNeighborEndOffset(u); k++) {
 					un = query.GetNeighbor(k);
 					if (!mark[un] && !inserted[un]) {
-						measure[un] = (cs.GetCandidateSize(un) - csPos[un]);
+						measure[un] = cs.GetCandidateSize(un) - csPos[un];
 						extVtx.insert(un);
 						inserted[un] = true;
 					}
@@ -164,8 +133,10 @@ void Backtrack::PrintAllMatches(const Graph& data, const Graph& query,
 		if (!embedded_u) { // backtracking
 			call = false;
 			csPos[u] = 0;
-			mark[u] = false;
-		}
-	}
 
+			measure[u] = cs.GetCandidateSize(u);
+			extVtx.insert(u);
+			inserted[u] = true;
+		}
+	} while (len > 0);
 }
--- test.sh
+++ test.sh
@@ -2,15 +2,15 @@
 
 for query in n1 n3 n5 n8 s1 s3 s5 s8
 do
-timeout 60 ./build/main/program ./data/lcc_yeast.igraph ./query/lcc_yeast_$query.igraph ./cs/cs_yeast_$query.igraph > ./embd/embd_yeast_$query.txt;
+timeout 60 ./build/main/program ./data/lcc_yeast.igraph ./query/lcc_yeast_$query.igraph ./cs/lcc_yeast_$query.cs > ./embd/embd_yeast_$query.txt;
 done
 
 for query in n1 n3 n5 n8 s1 s3 s5 s8
 do
-timeout 60 ./build/main/program ./data/lcc_human.igraph ./query/lcc_human_$query.igraph ./cs/cs_human_$query.igraph > ./embd/embd_human_$query.txt;
+timeout 60 ./build/main/program ./data/lcc_human.igraph ./query/lcc_human_$query.igraph ./cs/lcc_human_$query.cs > ./embd/embd_human_$query.txt;
 done
 
 for query in n1 n3 n5 n8 s1 s3 s5 s8
 do
-timeout 60 ./build/main/program ./data/lcc_hprd.igraph ./query/lcc_hprd_$query.igraph ./cs/cs_hprd_$query.igraph > ./embd/embd_hprd_$query.txt;
+timeout 60 ./build/main/program ./data/lcc_hprd.igraph ./query/lcc_hprd_$query.igraph ./cs/lcc_hprd_$query.cs > ./embd/embd_hprd_$query.txt;
 done
